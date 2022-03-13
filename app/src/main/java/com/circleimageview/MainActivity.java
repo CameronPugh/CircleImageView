da2f359e6ef1c18c959239c6f12ccaedc9ab8ea7
CameronPugh <cameron.pugh1@hotmail.com>
2022-03-13 00:55:43 +0000

    new

--- app/src/main/java/com/circleimageview/MainActivity.java
+++ app/src/main/java/com/circleimageview/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace  own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
